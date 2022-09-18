047c1bf447852a3f8bda457ab93fef6f488a0416
anna <unursey@gmail.com>
2022-09-19 01:27:39 +0300

    class

--- src/script.js
+++ src/script.js
@@ -1,9 +1,15 @@
-import { one } from "./script/main";
-
+import { Task } from "./script/main";
 import './scss/index.scss';
 
+
 const init = () => {
-  one();
+  
+const task = new Task('что-то', 6);
+console.log('task: ', task);
+console.log('taskName: ', task.taskName);
+console.log('changeTask: ', task.setTaskName('что-то еще'));
+console.log('taskName: ', task.taskName);
+console.log('changeTimer: ', task.changeTimer())
 };
 
 init();
--- src/script/main.js
+++ src/script/main.js
@@ -1,20 +1,52 @@
-export const one = () => {
-let count = 0;
-const imp = ["default", "important", "so-so"];
-document
-  .querySelector(".button-importance")
-  .addEventListener("click", ({ target }) => {
-    count += 1;
-    if (count >= imp.length) {
-      count = 0;
-    }
+// Ваша задача создать класс для реализации задачи
+// Принимает параметры название и счетчик (по умолчанию 0)
+// Полученный объект должен иметь:
 
-    for (let i = 0; i < imp.length; i++) {
-      if (count === i) {
-        target.classList.add(imp[i]);
-      } else {
-        target.classList.remove(imp[i]);
+// 1) уникальный ID (string)
+// 2) название (string)
+// 3) счетчик (number)
+
+// методы:
+// 1) Изменить значение счетчика (+1)
+// 2) Изменить имя
+
+export class Task {
+  constructor(taskName, timer = 0) {
+    this.id = String(performance.now()).replace('.', '');
+    this.taskName = String(taskName);
+    this.timer = Number(timer);
+  }
+
+  changeTimer() {
+    return (this.timer += 1);
+  }
+
+  setTaskName(taskName) {
+    this.taskName = taskName;
+    return this;
+  }
+}
+
+
+
+
+export const one = () => {
+  let count = 0;
+  const imp = ["default", "important", "so-so"];
+  document
+    .querySelector(".button-importance")
+    .addEventListener("click", ({ target }) => {
+      count += 1;
+      if (count >= imp.length) {
+        count = 0;
       }
-    }
-  });
+
+      for (let i = 0; i < imp.length; i++) {
+        if (count === i) {
+          target.classList.add(imp[i]);
+        } else {
+          target.classList.remove(imp[i]);
+        }
+      }
+    });
 };
